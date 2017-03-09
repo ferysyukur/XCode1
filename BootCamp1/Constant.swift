fdfb4c26cb0ce1bf9c5b7ee02d2fe3dbe275396e
Fery Syukur <ferysyukur26@gmail.com>
2017-03-09 17:45:39 +0800

    with Refresh Cotroller

---
+++ BootCamp1/Constant.swift
@@ -0,0 +1,10 @@
+//
+//  Constant.swift
+//  BootCamp1
+//
+//  Created by Fery Syukur on 3/9/17.
+//  Copyright © 2017 Fery Syukur. All rights reserved.
+//
+
+import Foundation
+let IMG_URL = "https://unsplash.it/200/300?image="
--- BootCamp1/ViewController.swift
+++ BootCamp1/ViewController.swift
@@ -6,13 +6,32 @@
 //  Copyright © 2017 Fery Syukur. All rights reserved.
 //
 
+struct Movies {
+    let title: String
+    let genre: String
+}
+
 import UIKit
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var cellMovie: UICollectionView!
+    var refreshControl : UIRefreshControl = UIRefreshControl()
+    var iImage = 1
     
-    //MARK: View LifeCycle
+    let arrayMovie: [Movies] = [
+        Movies(title: "Logan", genre: "Action"),
+        Movies(title: "Resident Evil", genre: "Horor"),
+        Movies(title: "Logan 2", genre: "Action"),
+        Movies(title: "Resident Evil 2", genre: "Horor"),
+        Movies(title: "Logan 3", genre: "Action"),
+        Movies(title: "Resident Evil 3", genre: "Horor"),
+        Movies(title: "Logan 2", genre: "Action"),
+        Movies(title: "Resident Evil 4", genre: "Horor"),
+        Movies(title: "Logan 3", genre: "Action"),
+        Movies(title: "Resident Evil 4", genre: "Horor")
+    ]
+    //MARK: - View LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -24,8 +43,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    //MARK: Configure
+    //MARK: - Configure
     func setupCollectionView(){
+        refreshControl.addTarget(self, action: #selector(handleRefresh(sender:)), for: .valueChanged)
+        cellMovie.addSubview(refreshControl)
         cellMovie.delegate = self
         let flowLayout = UICollectionViewFlowLayout()
         flowLayout.sectionInset = UIEdgeInsets(top: 8, left: 8, bottom: 0, right: 8)
@@ -37,6 +58,16 @@ class ViewController: UIViewController {
         let height : CGFloat = 100 + ((width / 3) * 4)
         flowLayout.itemSize = CGSize(width: width, height: height)
     }
+    
+    //MARK: - Action
+    func handleRefresh(sender: UIRefreshControl) {
+        iImage += 1
+        cellMovie.reloadData()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1){
+            self.cellMovie.reloadData()
+            self.refreshControl.endRefreshing()
+        }
+    }
 
 
 }
@@ -45,7 +76,16 @@ class ViewController: UIViewController {
 extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate{
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! MovieViewCell
+        
+        if let url = URL(string: "\(IMG_URL)\((indexPath.row + 1) * iImage)"){
+        cell.imgMovie.sd_setImage(with: url)
+        }
+        
+        let movie = arrayMovie[indexPath.item];
+        cell.lblTitle.text = movie.title
+        cell.lblDesc.text = movie.genre
+        
         return cell
     }
     
