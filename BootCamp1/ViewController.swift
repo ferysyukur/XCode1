b26132a6f001e5e12ccb15a4f580208a051781d5
Fery Syukur <ferysyukur26@gmail.com>
2017-03-09 10:28:10 +0800

    Perubahan 1

---
+++ BootCamp1/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BootCamp1
+//
+//  Created by Fery Syukur on 3/9/17.
+//  Copyright © 2017 Fery Syukur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let Coba1 = "Coba1"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
